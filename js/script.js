5afa5486d5a6f2d41ce2b9d0a9bf49eb3bdffb97
Emizzari <emilie@tonnessen.com>
2020-09-17 14:59:13 +0200

    fetch api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,25 @@
 import { url } from "./constants/api.js";
+import { createHTML } from "./components/createHTML.js";
+import { searchProducts } from "./components/searchProducts.js";
+import { displayMessage } from "./components/displayMessage.js";
+
+// Fetch API
+async function fetchAPI() {
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+
+        const product = json.data;
+
+        console.log(product);
+
+        createHTML(product);
+        searchProducts(product);
+
+    } catch (error) {
+        console.log(error);
+        displayMessage("error", error, ".product-container");
+    }
+}
+
+fetchAPI();
