70d91049b50766b1d6764d8133af8700eb98cf7a
Emizzari <emilie@tonnessen.com>
2020-09-18 11:42:55 +0200

    Display message if no result

--- js/components/createHTML.js
+++ js/components/createHTML.js
@@ -1,9 +1,16 @@
-export function createHTML(argument) {
+import { displayMessage } from "./displayMessage.js";
+import { emptyFilter } from "../constants/messages.js";
+
+export function createHTML(products) {
     const productContainer = document.querySelector(".product-container");
 
     productContainer.innerHTML = "";
 
-    argument.forEach(function (product) {
+    if (products.length === 0) {
+        displayMessage("", emptyFilter, ".product-container");
+    }
+
+    products.forEach(function (product) {
         productContainer.innerHTML += `
             <div class="product">
                 <h4>${product.name}</h4>
